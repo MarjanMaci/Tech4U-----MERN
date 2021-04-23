52eee4c04d93106c0bc20233417ac66172ee6441
MarjanMaci <marjanmarjanovski13@gmail.com>
2021-04-23 11:59:01 +0200

    Added database and fetching products from db

---
+++ backend/data/user.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name:"Admin user",
+        email:"admin@gmail.com",
+        password:bcrypt.hashSync('123456', 10),
+        isAdmin:true
+    },
+    {
+        name:"John Doe",
+        email:"john@gmail.com",
+        password:bcrypt.hashSync('123456', 10)
+    },
+    {
+        name:"Maci Macko",
+        email:"maci@gmail.com",
+        password:bcrypt.hashSync('123456', 10)
+    },
+]
+
+export default users
